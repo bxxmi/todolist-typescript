15eba93b232048d4702773d40307b1fb5675a4c5
bxxmi <bmi6638@gmail.com>
2022-03-01 22:26:32 +0900

    feat: implement add todo logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-const InputField: React.FC = () => {
+// 부모에게서 받은 props의 타입들을 지정한다.
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  onHandle: (e: React.FormEvent) => void;
+}
+
+// const InputField: React.FC<Props> = ({ todo, setTodo }) => { ... } 로도 타입 지정 가능
+const InputField = ({ todo, setTodo, onHandle }: Props) => {
+  // inputRef가 입력 요소임을 알도록 타입 지정
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input type="text" placeholder="Enter a task" className="input_box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        onHandle(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input_box"
+      />
       <button className="input_submit" type="submit">
         Go
       </button>
