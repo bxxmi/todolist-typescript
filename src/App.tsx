5785a977854a302d833d953bd5248ec4e144e774
bxxmi <bmi6638@gmail.com>
2022-03-01 22:25:46 +0900

    feat: implement add todo, show todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./model";
 
 // React.FC type: functional component를 의미
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  // model 디렉토리에서 생성된 interface를 사용할 것이고, 배열로 값이 들어가기 때문에 아래와 같은 구문을 사용
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodoList([...todoList, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} onHandle={handleAdd} />
+      <TodoList todoList={todoList} setTodoList={setTodoList} />
     </div>
   );
 };
